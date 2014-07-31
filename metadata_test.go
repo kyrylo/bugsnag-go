11c5515cd2b98c2628522274c1469d54ac572ad3
Kyrylo Silin <kyrylosilin@gmail.com>
2014-07-30 23:20:49 -0700

    Revert "metadata: add support for struct tag options"
    This reverts commit cdda3797418451848f3bc612187abee83341fe42.

--- metadata_test.go
+++ metadata_test.go
@@ -14,10 +14,8 @@ type _account struct {
 	Plan struct {
 		Premium bool
 	}
-	Password   string
-	secret     string
-	Email      string `json:"email"`
-	EmptyEmail string `json:"emptyemail,omitempty"`
+	Password string
+	secret   string
 }
 
 type _broken struct {
@@ -61,7 +59,6 @@ func TestMetaDataAdd(t *testing.T) {
 				"Premium": false,
 			},
 			"Password": "",
-			"email": "",
 		},
 	}) {
 		t.Errorf("metadata.Add didn't work: %#v", m)
@@ -108,8 +105,6 @@ func TestMetaDataSanitize(t *testing.T) {
 	account.Name = "test"
 	account.ID = "test"
 	account.secret = "hush"
-	account.Email = "example@example.com"
-	account.EmptyEmail = ""
 
 	m := MetaData{
 		"one": {
@@ -160,7 +155,6 @@ func TestMetaDataSanitize(t *testing.T) {
 					"Premium": false,
 				},
 				"Password": "[REDACTED]",
-				"email": "example@example.com",
 			},
 		},
 	}) {
